39062754443282599e4195e4b3ed8f776becb566
RafayelGardishyan <rgardishyan@gmail.com>
2019-05-27 16:38:53 +0200

    Added a new test to test wether the program can handle more than one object

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include "screen/screen.h"
 
 int main() {
-    screen s(10, 10);
-    for (int i = 0; i < 10; ++i) {
+    screen s(20, 20);
+    for (int i = 0; i < 20; ++i) {
         s.add_object(screenobject(i, i, "@"));
+        s.add_object(screenobject(20 - i, 20 - i, "*"));
+
         s.render();
     }
 }
